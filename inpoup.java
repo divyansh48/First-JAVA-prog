ab4cc411eb6d8dfd82ac1eb5d87a1e193819dee0
divyansh <imdivyansh.04@gmail.com>
2024-04-12 12:55:59 +0530

    main.java file is added

---
+++ inpoup.java
@@ -0,0 +1,14 @@
+package com.divyansh;
+
+import java.util.Scanner;
+
+public class inpoup {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+
+
+
+    }
+}
